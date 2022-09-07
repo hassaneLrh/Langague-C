2cc4627d77e88cf71be15400ed9b243b0935f43d
unknown <larhouatihassane8@gmail.com>
2022-09-07 11:47:47 +0100

    premiers commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    char charachtername[] = "Hassane";
+    int charachterAge = 19;
+    printf("hello i'm Hassane AND I'm 19 years old\n", charachtername, charachterAge);
+    printf("hello i'm Hassane and i'm 19 years old\n", charachtername,charachterAge);
+    printf("my favorite %s is %f" , "number" , 67.89);
+
+    return 0;
+}
